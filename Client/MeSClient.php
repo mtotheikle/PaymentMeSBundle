7fb7697eb12878c8d75b7f80804c6d9329832c68
Jan Michael Tan <jmgtan@gmail.com>
2013-01-24 11:20:52 +0800

    Added methods for auth, void, refund, capture Added the corresponding methods to the plugin Add tests

--- Client/MeSClient.php
+++ Client/MeSClient.php
@@ -6,10 +6,17 @@ use ImmersiveLabs\PaymentMeSBundle\PaymentGateway\Trident;
 
 class MeSClient
 {
+    const TXN_TYPE_SALE = 'sale';
+    const TXN_TYPE_PRE_AUTH = 'pre-auth';
+
     protected $profileId;
     protected $profileKey;
     protected $apiUrl;
 
+    protected $txnMappings = array(
+        self::TXN_TYPE_SALE => 'ImmersiveLabs\PaymentMeSBundle\PaymentGateway\Trident\TpgSale',
+        self::TXN_TYPE_PRE_AUTH => 'ImmersiveLabs\PaymentMeSBundle\PaymentGateway\Trident\TpgPreAuth'
+    );
     public function __construct($auth)
     {
         list($this->profileId, $this->profileKey, $this->apiUrl) = $auth;
@@ -17,16 +24,90 @@ class MeSClient
 
     public function postSale($cardNumber, $expirationMonth, $expirationYear, $amount)
     {
-        $sale = new Trident\TpgSale($this->profileId, $this->profileKey);
+        return $this->adhocTxnExecute(self::TXN_TYPE_SALE, $cardNumber, $expirationMonth, $expirationYear, $amount);
+    }
 
-        $sale->setTransactionData($cardNumber, $expirationMonth . $expirationYear, $amount);
-        $sale->execute();
+    public function postSaleForStoredData($cardId, $amount)
+    {
+        return $this->storedDataTxnExecute(self::TXN_TYPE_SALE, $cardId, $amount);
+    }
 
-        if (!$sale->isApproved()) {
+    public function postPreAuth($cardNumber, $expirationMonth, $expirationYear, $amount)
+    {
+        return $this->adhocTxnExecute(self::TXN_TYPE_PRE_AUTH, $cardNumber, $expirationMonth, $expirationYear, $amount);
+    }
+
+    public function postPreAuthForStoredData($cardId, $amount)
+    {
+        return $this->storedDataTxnExecute(self::TXN_TYPE_PRE_AUTH, $cardId, $amount);
+    }
+
+    public function postSettle($transactionId, $amount)
+    {
+        $settle = new Trident\TpgSettle($this->profileId, $this->profileKey, $transactionId, $amount);
+
+        $settle->execute();
+
+        if (!$settle->isApproved()) {
             return false;
         }
 
+        return $settle->ResponseFields['transaction_id'];
+    }
 
-        return $sale->ResponseFields['auth_code'];
+    public function postVoid($transactionId)
+    {
+        $void = new Trident\TpgVoid($this->profileId, $this->profileKey, $transactionId);
+        $void->execute();
+
+        if (!$void->isApproved()) {
+            return false;
+        }
+
+        return $void->ResponseFields['transaction_id'];
+    }
+
+    public function postRefund($transactionId)
+    {
+        $refund = new Trident\TpgRefund($this->profileId, $this->profileKey, $transactionId);
+        $refund->execute();
+
+        if (!$refund->isApproved()) {
+            return false;
+        }
+
+        return $refund->ResponseFields['transaction_id'];
+    }
+
+    protected function storedDataTxnExecute($txnType, $cardId, $amount)
+    {
+        $txnClass = $this->txnMappings[$txnType];
+
+        $txn = new $txnClass($this->profileId, $this->profileKey);
+
+        $txn->setStoredData($cardId, $amount);
+        $txn->execute();
+
+        if (!$txn->isApproved()) {
+            return false;
+        }
+
+        return $txn->ResponseFields['transaction_id'];
+    }
+
+    protected function adhocTxnExecute($txnType, $cardNumber, $expirationMonth, $expirationYear, $amount)
+    {
+        $txnClass = $this->txnMappings[$txnType];
+
+        $txn = new $txnClass($this->profileId, $this->profileKey);
+
+        $txn->setTransactionData($cardNumber, $expirationMonth . $expirationYear, $amount);
+        $txn->execute();
+
+        if (!$txn->isApproved()) {
+            return false;
+        }
+
+        return $txn->ResponseFields['transaction_id'];
     }
 }
